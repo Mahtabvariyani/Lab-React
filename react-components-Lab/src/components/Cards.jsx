4a3f4bf9d95ec91a8876b1ec40f4b3b3cd2538ed
MAH <108659794+Mahtabvariyani@users.noreply.github.com>
2023-09-19 10:06:24 +0200

    Update Cards.jsx

--- react-components-Lab/src/components/Cards.jsx
+++ react-components-Lab/src/components/Cards.jsx
@@ -4,14 +4,14 @@ import img2 from "../assets/icon2.png";
 import img3 from "../assets/icon3.png";
 import img4 from "../assets/icon4.png";
 
-function Cards((icon,alt,title,description){
-  return(
-    <div>
-    <h1></h1>
-    
+function Cards({icon,alt,title,description}){
+  return (
+    <div className="card">
+      <img src={icon} alt={alt} />
+      <h4 className="card-title"> {title} </h4>
+      <p> {description} </p>
     </div>
-    
-  )
+  );
 }
 
 https://codesandbox.io/s/react-lab-say-hello-to-reactjs-hzwhkh?file=/src/App.js
