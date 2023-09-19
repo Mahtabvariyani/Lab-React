1060bb992d6fa9201fbdfe192999330e00857af9
MAH <108659794+Mahtabvariyani@users.noreply.github.com>
2023-09-19 09:40:37 +0200

    Update Cards.jsx

--- react-components-Lab/src/components/Cards.jsx
+++ react-components-Lab/src/components/Cards.jsx
@@ -4,64 +4,16 @@ import img2 from "../assets/icon2.png";
 import img3 from "../assets/icon3.png";
 import img4 from "../assets/icon4.png";
 
-class Cards extends React.Component {
-  render() {
-    return (
-      <div className="card-container">
-        <div className="card">
-          <img className="card-img-top" src={img1} alt="Card image cap" />
-          <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">
-              Some quick example text to build on the card title and make up the bulk of the card's content.
-            </p>
-            <a href="#" className="btn btn-primary">
-              Go somewhere
-            </a>
-          </div>
-        </div>
-
-        <div className="card">
-          <img className="card-img-top" src={img2} alt="Card image cap" />
-          <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">
-              Some quick example text to build on the card title and make up the bulk of the card's content.
-            </p>
-            <a href="#" className="btn btn-primary">
-              Go somewhere
-            </a>
-          </div>
-        </div>
-
-        <div className="card">
-          <img className="card-img-top" src={img3} alt="Card image cap" />
-          <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">
-              Some quick example text to build on the card title and make up the bulk of the card's content.
-            </p>
-            <a href="#" className="btn btn-primary">
-              Go somewhere
-            </a>
-          </div>
-        </div>
-
-        <div className="card">
-          <img className="card-img-top" src={img4} alt="Card image cap" />
-          <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">
-              Some quick example text to build on the card title and make up the bulk of the card's content.
-            </p>
-            <a href="#" className="btn btn-primary">
-              Go somewhere
-            </a>
-          </div>
-        </div>
-      </div>
-    );
-  }
+function Cards((icon,alt,title,description){
+  return(
+    <div>
+    <h1></h1>
+    
+    </div>
+    
+  )
 }
+
+https://codesandbox.io/s/react-lab-say-hello-to-reactjs-hzwhkh?file=/src/App.js
 
 export default Cards;
